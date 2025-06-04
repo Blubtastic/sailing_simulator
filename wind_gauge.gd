9e63f22cba5c04ac64826e8d2390237b1af71dd0
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2025-06-04 09:01:58 +0200

    feat: wind HUD below ship WIP

---
+++ wind_gauge.gd
@@ -0,0 +1,11 @@
+extends Node3D
+@onready var raycast: RayCast3D = $RayCast3D
+@onready var wind_pointer: Node3D = $WindPointer
+
+
+func _physics_process(delta: float) -> void:
+	var wind_angle = Globals.wind_direction - rotation_degrees.y
+	if raycast.is_colliding():
+		var hit_position = raycast.get_collision_point()
+		wind_pointer.global_position = hit_position
+		print("Hit Position: ", hit_position)
