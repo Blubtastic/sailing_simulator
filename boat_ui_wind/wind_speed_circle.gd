051fc35ffecf9ec780f039cc2749706f23ac9e8d
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2025-06-17 09:31:22 +0200

    feat: use wind_speed_circle speed for player

--- boat_ui_wind/wind_speed_circle.gd
+++ boat_ui_wind/wind_speed_circle.gd
@@ -4,15 +4,20 @@ extends Node3D
 @onready var arrow_head: Node3D = $ArrowHead
 @onready var draw_line: MeshInstance3D = $DrawLine
 
-func _physics_process(_delta: float) -> void:
+var speed = 0
 
+func _physics_process(_delta: float) -> void:
 	if raycast.is_colliding():
 		var hit_position = raycast.get_collision_point()
-		#print(raycast.get_collider())
-		var speed = hit_position.distance_to(global_position)
-		arrow_head.global_position = hit_position
-		var speedSize = speed*0.1
-		var speedSizeExp = speedSize*speedSize
-		arrow_head.scale = Vector3(speedSizeExp, speedSizeExp, speedSizeExp)
-		draw_line.draw_line(hit_position)
+		var distance = hit_position.distance_to(global_position)
+		speed = distance if distance > 7 else 0
 		
+		# Display visuals
+		var arrow_scale = (speed*0.1)*(speed*0.1)
+		arrow_head.scale = Vector3(arrow_scale, arrow_scale, arrow_scale)
+		var visual_position = hit_position if speed > 0 else global_position
+		arrow_head.global_position = visual_position
+		draw_line.draw_line(visual_position)
+
+func get_speed():
+	return speed
--- player.gd
+++ player.gd
@@ -4,18 +4,23 @@ extends CharacterBody3D
 
 const ACCELLERATE_VEC = Vector3(0, 0, -10)
 const ROTATE_ANGLE = 3
-const ACCELERATION = 0.2
-const MAX_SPEED = 10
 const MAX_WIND_ANGLE = 35
+var max_speed = 10
+var acceleration = 0.2
 var current_velocity = 0
+
+# TODO: use wind_speed_circle.get_speed() to calculate wind speed.
+# Remove MAX_WIND_ANGLE. The player only knows about the speed from wind_speed_circle
+# and calculates its speed from that.
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
 		rotation.y += ROTATE_ANGLE * delta
 	if Input.is_action_pressed("ui_right"):
 		rotation.y -= ROTATE_ANGLE * delta
-		
 	move_and_slide()
+	max_speed = wind_speed_circle.get_speed()
+	print(max_speed)
 	
 	var velocity_direction = global_transform.basis.z
 	#var velocity_increase = ACCELLERATE_VEC.rotated(Vector3.UP, rotation.y) * delta * 1
@@ -23,11 +28,11 @@ func _physics_process(delta: float) -> void:
 	var wind_angle = Globals.wind_direction - rotation_degrees.y
 	var is_headwind = abs(wind_angle) < MAX_WIND_ANGLE
 	if !is_headwind:
-		current_velocity = clamp(0, current_velocity+ACCELERATION, MAX_SPEED)
+		current_velocity = clamp(0, current_velocity+acceleration, max_speed)
 		velocity = velocity_direction * -current_velocity
 	else:
 		if current_velocity > 0:
-			current_velocity = clamp(0, current_velocity-ACCELERATION*2, MAX_SPEED)
+			current_velocity = clamp(0, current_velocity-acceleration*2, max_speed)
 		else:
 			current_velocity = 0
 		velocity = velocity_direction * -current_velocity
