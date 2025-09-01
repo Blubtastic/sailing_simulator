e0babb2a63f6ce754ea2e905238ac806e42312f4
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2025-09-01 08:26:14 +0200

    feat: improve placement of water particles

--- player/water_trail/water_foam.gd
+++ player/water_trail/water_foam.gd
@@ -1,4 +1,11 @@
 extends Node3D
 
+@export var player: Player
+@onready var gpu_particles_3d: GPUParticles3D = $GPUParticles3D
+
 func _process(_delta: float) -> void:
 	global_rotation_degrees.z = 0
+	
+	#var ratio = (1 - (player.velocity.length() / 14)) / 1
+	#gpu_particles_3d.interp_to_end = ratio
+	#print(ratio)
