23244f2d79bf28d27d7bef1e67e5e0c46f69014b
Martin Storødegård <17567077+Blubtastic@users.noreply.github.com>
2025-09-02 08:20:35 +0200

    feat: animate water trails on side

--- player/water_trail/water_foam.gd
+++ player/water_trail/water_foam.gd
@@ -6,6 +6,6 @@ extends Node3D
 func _process(_delta: float) -> void:
 	global_rotation_degrees.z = 0
 	
-	#var ratio = (1 - (player.velocity.length() / 14)) / 1
-	#gpu_particles_3d.interp_to_end = ratio
-	#print(ratio)
+	# Fade out particles when slow / standstill
+	var ratio = (player.velocity.length() / 10) / 1
+	gpu_particles_3d.amount_ratio = ratio
--- player/water_trail/water_trail.gd
+++ player/water_trail/water_trail.gd
@@ -1,4 +1,11 @@
 extends Node3D
 
+@export var player: Player
+@onready var cpu_particles_3d: CPUParticles3D = $CPUParticles3D
+
 func _process(_delta: float) -> void:
 	global_rotation_degrees.z = 0
+	
+	# Fade out particles when slow / standstill
+	#var ratio = (player.velocity.length() / 10) / 1
+	#gpu_particles_3d.amount_ratio = ratio
